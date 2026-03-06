9561520e45120c06f543f92dfb33052049a502ed
Stella Douka <35050451+stelladk@users.noreply.github.com>
2026-03-06 21:51:07 +0100

    train from scratch

--- train.py
+++ train.py
@@ -1,180 +1,232 @@
+import argparse
+import glob
+import logging
 import os
 import sys
 import time
-import glob
+
 import numpy as np
 import torch
-import utils
-import logging
-import argparse
-import torch.nn as nn
-import genotypes
-import torch.utils
-import torchvision.datasets as dset
 import torch.backends.cudnn as cudnn
-
+import torch.nn as nn
 from torch.autograd import Variable
+
+import utils
+from dataset import get_dataset, get_val_dataset
 from model import NetworkCIFAR as Network
 
+# ───────────────────────────────────────────── argument parsing ─────── #
+parser = argparse.ArgumentParser("PC-DARTS evaluation – custom dataset")
 
-parser = argparse.ArgumentParser("cifar")
-parser.add_argument('--data', type=str, default='../data', help='location of the data corpus')
-parser.add_argument('--set', type=str, default='cifar10', help='location of the data corpus')
-parser.add_argument('--batch_size', type=int, default=96, help='batch size')
-parser.add_argument('--learning_rate', type=float, default=0.025, help='init learning rate')
-parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
-parser.add_argument('--weight_decay', type=float, default=3e-4, help='weight decay')
-parser.add_argument('--report_freq', type=float, default=50, help='report frequency')
-parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
-parser.add_argument('--epochs', type=int, default=600, help='num of training epochs')
-parser.add_argument('--init_channels', type=int, default=36, help='num of init channels')
-parser.add_argument('--layers', type=int, default=20, help='total number of layers')
-parser.add_argument('--model_path', type=str, default='saved_models', help='path to save the model')
-parser.add_argument('--auxiliary', action='store_true', default=False, help='use auxiliary tower')
-parser.add_argument('--auxiliary_weight', type=float, default=0.4, help='weight for auxiliary loss')
-parser.add_argument('--cutout', action='store_true', default=False, help='use cutout')
-parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
-parser.add_argument('--drop_path_prob', type=float, default=0.3, help='drop path probability')
-parser.add_argument('--save', type=str, default='EXP', help='experiment name')
-parser.add_argument('--seed', type=int, default=0, help='random seed')
-parser.add_argument('--arch', type=str, default='PCDARTS', help='which architecture to use')
-parser.add_argument('--grad_clip', type=float, default=5, help='gradient clipping')
+# data
+parser.add_argument("--data", type=str, default="../data")
+parser.add_argument(
+    "--dataset",
+    type=str,
+    default="cifar10",
+    help="cifar10 | cifar100 | imagefolder | addnist | multnist | …",
+)
+parser.add_argument("--image_size", type=int, default=None)
+parser.add_argument("--num_classes", type=int, default=None)
+parser.add_argument(
+    "--dataset_mean", type=float, nargs=3, default=None, metavar=("R", "G", "B")
+)
+parser.add_argument(
+    "--dataset_std", type=float, nargs=3, default=None, metavar=("R", "G", "B")
+)
+parser.add_argument("--grayscale", action="store_true")
+parser.add_argument(
+    "--no_augment",
+    action="store_true",
+    default=False,
+    help="Disable all training-time augmentation",
+)
+
+# NpyWebDataset-specific
+parser.add_argument("--npyweb_url", type=str, default=None)
+parser.add_argument("--npyweb_name", type=str, default="")
+parser.add_argument("--npyweb_root", type=str, default="data/webdatasets/npy")
+parser.add_argument("--npyweb_data_key", type=str, default="_x")
+parser.add_argument("--npyweb_label_key", type=str, default="_y")
+parser.add_argument("--npyweb_preload", action="store_true", default=True)
+
+# training
+parser.add_argument("--batch_size", type=int, default=96)
+parser.add_argument("--learning_rate", type=float, default=0.025)
+parser.add_argument("--momentum", type=float, default=0.9)
+parser.add_argument("--weight_decay", type=float, default=3e-4)
+parser.add_argument("--report_freq", type=int, default=50)
+parser.add_argument("--epochs", type=int, default=600)
+parser.add_argument("--grad_clip", type=float, default=5)
+
+# model
+parser.add_argument("--gpu", type=int, default=0)
+parser.add_argument("--init_channels", type=int, default=36)
+parser.add_argument("--layers", type=int, default=20)
+parser.add_argument("--auxiliary", action="store_true", default=False)
+parser.add_argument("--auxiliary_weight", type=float, default=0.4)
+parser.add_argument("--cutout", action="store_true", default=False)
+parser.add_argument("--cutout_length", type=int, default=16)
+parser.add_argument("--drop_path_prob", type=float, default=0.3)
+parser.add_argument(
+    "--arch", type=str, default="PCDARTS", help="genotype name from genotypes.py"
+)
+parser.add_argument("--save", type=str, default="EXP")
+parser.add_argument("--seed", type=int, default=0)
+
 args = parser.parse_args()
+args.save = "eval-{}-{}".format(args.save, time.strftime("%Y%m%d-%H%M%S"))
+utils.create_exp_dir(args.save, scripts_to_save=glob.glob("*.py"))
 
-args.save = 'eval-{}-{}'.format(args.save, time.strftime("%Y%m%d-%H%M%S"))
-utils.create_exp_dir(args.save, scripts_to_save=glob.glob('*.py'))
-
-log_format = '%(asctime)s %(message)s'
-logging.basicConfig(stream=sys.stdout, level=logging.INFO,
-    format=log_format, datefmt='%m/%d %I:%M:%S %p')
-fh = logging.FileHandler(os.path.join(args.save, 'log.txt'))
+log_format = "%(asctime)s %(message)s"
+logging.basicConfig(
+    stream=sys.stdout, level=logging.INFO, format=log_format, datefmt="%m/%d %I:%M:%S %p"
+)
+fh = logging.FileHandler(os.path.join(args.save, "log.txt"))
 fh.setFormatter(logging.Formatter(log_format))
 logging.getLogger().addHandler(fh)
 
-CIFAR_CLASSES = 10
 
-if args.set=='cifar100':
-    CIFAR_CLASSES = 100
+# ───────────────────────────────────────────────────────── main ─────── #
 def main():
-  if not torch.cuda.is_available():
-    logging.info('no gpu device available')
-    sys.exit(1)
+    if not torch.cuda.is_available():
+        logging.info("No GPU available, exiting.")
+        sys.exit(1)
 
-  np.random.seed(args.seed)
-  torch.cuda.set_device(args.gpu)
-  cudnn.benchmark = True
-  torch.manual_seed(args.seed)
-  cudnn.enabled=True
-  torch.cuda.manual_seed(args.seed)
-  logging.info('gpu device = %d' % args.gpu)
-  logging.info("args = %s", args)
+    np.random.seed(args.seed)
+    torch.cuda.set_device(args.gpu)
+    cudnn.benchmark = True
+    torch.manual_seed(args.seed)
+    cudnn.enabled = True
+    torch.cuda.manual_seed(args.seed)
+    logging.info("gpu device = %d", args.gpu)
+    logging.info("args = %s", args)
 
-  genotype = eval("genotypes.%s" % args.arch)
-  model = Network(args.init_channels, CIFAR_CLASSES, args.layers, args.auxiliary, genotype)
-  model = model.cuda()
+    # ── dataset ────────────────────────────────────────────────────────
+    train_data, n_classes, in_channels = get_dataset(args)
+    valid_data = get_val_dataset(args)
+    logging.info(
+        "Dataset=%s  classes=%d  in_channels=%d", args.dataset, n_classes, in_channels
+    )
 
-  logging.info("param size = %fMB", utils.count_parameters_in_MB(model))
+    train_queue = torch.utils.data.DataLoader(
+        train_data,
+        batch_size=args.batch_size,
+        shuffle=True,
+        pin_memory=True,
+        num_workers=2,
+    )
 
-  criterion = nn.CrossEntropyLoss()
-  criterion = criterion.cuda()
-  optimizer = torch.optim.SGD(
-      model.parameters(),
-      args.learning_rate,
-      momentum=args.momentum,
-      weight_decay=args.weight_decay
-      )
+    valid_queue = torch.utils.data.DataLoader(
+        valid_data,
+        batch_size=args.batch_size,
+        shuffle=False,
+        pin_memory=True,
+        num_workers=2,
+    )
 
-  train_transform, valid_transform = utils._data_transforms_cifar10(args)
-  if args.set=='cifar100':
-      train_data = dset.CIFAR100(root=args.data, train=True, download=True, transform=train_transform)
-      valid_data = dset.CIFAR100(root=args.data, train=False, download=True, transform=valid_transform)
-  else:
-      train_data = dset.CIFAR10(root=args.data, train=True, download=True, transform=train_transform)
-      valid_data = dset.CIFAR10(root=args.data, train=False, download=True, transform=valid_transform)
-  #train_data = dset.CIFAR10(root=args.data, train=True, download=True, transform=train_transform)
-  #valid_data = dset.CIFAR10(root=args.data, train=False, download=True, transform=valid_transform)
+    # ── model ──────────────────────────────────────────────────────────
+    genotype = eval("genotypes.%s" % args.arch)
+    model = Network(args.init_channels, n_classes, args.layers, args.auxiliary, genotype)
+    model = model.cuda()
+    logging.info("param size = %.2f MB", utils.count_parameters_in_MB(model))
 
-  train_queue = torch.utils.data.DataLoader(
-      train_data, batch_size=args.batch_size, shuffle=True, pin_memory=True, num_workers=2)
+    criterion = nn.CrossEntropyLoss().cuda()
+    optimizer = torch.optim.SGD(
+        model.parameters(),
+        args.learning_rate,
+        momentum=args.momentum,
+        weight_decay=args.weight_decay,
+    )
 
-  valid_queue = torch.utils.data.DataLoader(
-      valid_data, batch_size=args.batch_size, shuffle=False, pin_memory=True, num_workers=2)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, args.epochs)
 
-  scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, float(args.epochs))
-  best_acc = 0.0
-  for epoch in range(args.epochs):
-    scheduler.step()
-    logging.info('epoch %d lr %e', epoch, scheduler.get_lr()[0])
-    model.drop_path_prob = args.drop_path_prob * epoch / args.epochs
+    # ── loop ───────────────────────────────────────────────────────────
+    best_acc = 0.0
+    for epoch in range(args.epochs):
+        scheduler.step()
+        logging.info("epoch %d  lr %e", epoch, scheduler.get_last_lr()[0])
+        model.drop_path_prob = args.drop_path_prob * epoch / args.epochs
 
-    train_acc, train_obj = train(train_queue, model, criterion, optimizer)
-    logging.info('train_acc %f', train_acc)
+        train_acc, _ = train_one_epoch(train_queue, model, criterion, optimizer)
+        logging.info("train_acc %f", train_acc)
 
-    valid_acc, valid_obj = infer(valid_queue, model, criterion)
-    if valid_acc > best_acc:
-        best_acc = valid_acc
-    logging.info('valid_acc %f, best_acc %f', valid_acc, best_acc)
+        valid_acc, _ = infer(valid_queue, model, criterion)
+        if valid_acc > best_acc:
+            best_acc = valid_acc
+        logging.info("valid_acc %f  best_acc %f", valid_acc, best_acc)
 
-    utils.save(model, os.path.join(args.save, 'weights.pt'))
+        utils.save(model, os.path.join(args.save, "weights.pt"))
 
 
-def train(train_queue, model, criterion, optimizer):
-  objs = utils.AvgrageMeter()
-  top1 = utils.AvgrageMeter()
-  top5 = utils.AvgrageMeter()
-  model.train()
+def train_one_epoch(train_queue, model, criterion, optimizer):
+    objs = utils.AvgrageMeter()
+    top1 = utils.AvgrageMeter()
+    top5 = utils.AvgrageMeter()
+    model.train()
 
-  for step, (input, target) in enumerate(train_queue):
-    input = Variable(input).cuda()
-    target = Variable(target).cuda(async=True)
+    for step, (x, y) in enumerate(train_queue):
+        x = Variable(x).cuda()
+        y = Variable(y).cuda()
 
-    optimizer.zero_grad()
-    logits, logits_aux = model(input)
-    loss = criterion(logits, target)
-    if args.auxiliary:
-      loss_aux = criterion(logits_aux, target)
-      loss += args.auxiliary_weight*loss_aux
-    loss.backward()
-    nn.utils.clip_grad_norm(model.parameters(), args.grad_clip)
-    optimizer.step()
+        optimizer.zero_grad()
+        logits, logits_aux = model(x)
+        loss = criterion(logits, y)
+        if args.auxiliary:
+            loss += args.auxiliary_weight * criterion(logits_aux, y)
+        loss.backward()
+        nn.utils.clip_grad_norm_(model.parameters(), args.grad_clip)
+        optimizer.step()
 
-    prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
-    n = input.size(0)
-    objs.update(loss.data[0], n)
-    top1.update(prec1.data[0], n)
-    top5.update(prec5.data[0], n)
+        p1, p5 = utils.accuracy(logits, y, topk=(1, 5))
+        n = x.size(0)
+        objs.update(loss.item(), n)
+        top1.update(p1.item(), n)
+        top5.update(p5.item(), n)
 
-    if step % args.report_freq == 0:
-      logging.info('train %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
+        if step % args.report_freq == 0:
+            logging.info(
+                "train %03d  loss=%.4f  top1=%.2f  top5=%.2f",
+                step,
+                objs.avg,
+                top1.avg,
+                top5.avg,
+            )
 
-  return top1.avg, objs.avg
+    return top1.avg, objs.avg
 
 
 def infer(valid_queue, model, criterion):
-  objs = utils.AvgrageMeter()
-  top1 = utils.AvgrageMeter()
-  top5 = utils.AvgrageMeter()
-  model.eval()
+    objs = utils.AvgrageMeter()
+    top1 = utils.AvgrageMeter()
+    top5 = utils.AvgrageMeter()
+    model.eval()
 
-  for step, (input, target) in enumerate(valid_queue):
-    input = Variable(input, volatile=True).cuda()
-    target = Variable(target, volatile=True).cuda(async=True)
+    with torch.no_grad():
+        for step, (x, y) in enumerate(valid_queue):
+            x = Variable(x).cuda()
+            y = Variable(y).cuda()
 
-    logits, _ = model(input)
-    loss = criterion(logits, target)
+            logits, _ = model(x)
+            loss = criterion(logits, y)
 
-    prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
-    n = input.size(0)
-    objs.update(loss.data[0], n)
-    top1.update(prec1.data[0], n)
-    top5.update(prec5.data[0], n)
+            p1, p5 = utils.accuracy(logits, y, topk=(1, 5))
+            n = x.size(0)
+            objs.update(loss.item(), n)
+            top1.update(p1.item(), n)
+            top5.update(p5.item(), n)
 
-    if step % args.report_freq == 0:
-      logging.info('valid %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
+            if step % args.report_freq == 0:
+                logging.info(
+                    "valid %03d  loss=%.4f  top1=%.2f  top5=%.2f",
+                    step,
+                    objs.avg,
+                    top1.avg,
+                    top5.avg,
+                )
 
-  return top1.avg, objs.avg
+    return top1.avg, objs.avg
 
 
-if __name__ == '__main__':
-  main() 
-
+if __name__ == "__main__":
+    main()
